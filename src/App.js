c928072472755f70cb3e7e602b94fb497d8fa764
Rashford <mikearowo@gmail.com>
2023-02-15 16:13:48 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Person } from "./components/Person";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Person
+        name="Rashford"
+        email="rashford@gmail.com"
+        age={21}
+        isMarried={true}
+        friends={["Jessica", "Jake", "Jerry", "Jasmine"]}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Person.js
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+export const Person = (props) => {
+  return (
+    <div>
+      <h1>Name: {props.name}</h1>
+      <h1>Email: {props.email}</h1>
+      <h1>Age: {props.age}</h1>
+      <h1>This Person {props.isMarried ? "Is" : "Is not"} MARRIED</h1>
+      {props.Ffriends.map((friend) => (
+        <h1>{friend}</h1>
+      ))}
+    </div>
+  );
+};
+
+Person.propTypes = {
+  name: PropTypes.string,
+  email: PropTypes.string,
+  age: PropTypes.number,
+  isMarried: PropTypes.bool,
+  friends: PropTypes.arrayOf(PropTypes.string),
+};
